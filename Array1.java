4a4e35d73a053bccaf036b6e4d520a0b0612f5ef
Abdul1042 <110098746+Abdul1042@users.noreply.github.com>
2022-08-01 19:34:45 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,14 @@
+
+public class Array1 {
+	public static void main(String args[]) {
+		int array[]= {5,10,15,20,25};
+		for(int i=0;i<5;i++) {
+		System.out.println("Elements of array is: "+array[i]);
+		}
+		int a=3,b=3;
+		int[][] Multi = {{2, 4, 6}, {3, 6, 9} };
+		      
+		      System.out.println("\nLength of row 1: " + Multi[0].length);
+		      }
+
+}
